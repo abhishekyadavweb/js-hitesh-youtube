a70a09d1ea607e0856ed307910681f33552f4b0d
abhishekyadavweb <138349234+abhishekyadavweb@users.noreply.github.com>
2023-08-01 05:54:18 +0000

    variable configure done

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = "15652"
+let accountName = 'hitesh'
+var accountEmail = 'hitesh@gmail.com'
+accountCity = 'mumbai'
+
+// accountId = '2' ('not allowed')
+
+accountName = 'abhi'
+accountEmail = 'abhi@gmail.com'
+accountCity = 'bandra'
+
+console.table([accountId, accountName, accountEmail, accountCity])
