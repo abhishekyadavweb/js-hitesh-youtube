8ce4259e9d51497ffd1356d8f72da1518b753e7f
abhishekyadavweb <138349234+abhishekyadavweb@users.noreply.github.com>
2024-09-01 17:22:12 +0000

    days 3

---
+++ 03_days/acty_1.js
@@ -0,0 +1,15 @@
+const num = 5;
+if (num === 0 ) {
+    console.log(`Number is ${num}`);
+} else if (num > 0) {
+    console.log(`${num} is positive`);
+} else {
+    console.log(`${num} is negative`);
+} 
+
+const age = 15;
+if (age >= 18) {
+    console.log(`The Number is ${age}, you are eligible to vote`);
+} else {
+    console.log(`The Number is ${age}, you are not eligible to vote `);
+    }
---
+++ 03_days/acty_2.js
@@ -0,0 +1,8 @@
+const num = 5;
+if (num === 0 ) {
+    console.log(`Number is ${num}`);
+} else if (num > 0) {
+    console.log(`${num} is positive`);
+} else {
+    console.log(`${num} is negative`);
+} 
