f34a59fd806f78cc2e9afa993cc511f6f92d29da
abhishekyadavweb <138349234+abhishekyadavweb@users.noreply.github.com>
2024-09-07 17:56:49 +0000

    days 4

---
+++ 04_days/acty_2.js
@@ -0,0 +1,16 @@
+let sum = 0;
+let i = 1;
+while (i <= 10) {
+    sum += i;
+    i++;
+}
+// console.log(`sum of 1 to 10: ${i}`);
+
+
+//****************task 2 ************* */
+
+j = 10
+while (j > 0) {
+    console.log(j);
+    j--
+}
---
+++ 04_days/acty_5.js
@@ -0,0 +1,16 @@
+for (let i = 0; i < 10; i++) {
+    if (i == 5) {
+        continue;
+        }
+    // console.log(i);
+}
+
+//************task 2 *********** */
+
+for (let j = 0; j < 10; j++) {
+    if (j == 7) {
+        break;
+    }
+    console.log(j);
+    
+}
