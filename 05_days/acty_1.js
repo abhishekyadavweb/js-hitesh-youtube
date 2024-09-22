a895f6800a82429ea5238681aad31581d1a959c4
abhishekyadavweb <138349234+abhishekyadavweb@users.noreply.github.com>
2024-09-22 17:49:53 +0000

    days 4

---
+++ 05_days/acty_1.js
@@ -0,0 +1,15 @@
+function checkNum(num) {
+    if (num % 2 === 0) {
+        console.log("number is odd");
+        return true;
+    }
+console.log("numbert is even");
+}
+checkNum(44)
+
+// ******************task 2***********************
+
+function squareNum(number) {
+    console.log(number * number);
+}
+squareNum(5)
---
+++ 05_days/acty_2.js
@@ -0,0 +1,11 @@
+function maxNum(number1, number2) {
+    return Math.max(number1, number2)
+}
+console.log(maxNum(60, 75));
+
+//*************** task 2 *********** */
+
+function concat(str1, str2 ) {
+console.log(str1 + str2)
+}
+concat("hello,", " world!")
